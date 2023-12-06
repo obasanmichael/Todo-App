373100815dbe4fd8b6215008c59b727d68796d79
Obasan Tolulope <obasantolu@gmail.com>
2023-12-06 01:58:00 +0100

    Adjusted the Header component

--- src/components/ToDo/Header.tsx
+++ src/components/ToDo/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return <div className="max-w-[675px] mx-auto">
-    <h1 className="uppercase font-bold tracking-[5px] text-white text-[36px] md:px-[24px]">todo</h1>
+    <h1 className="uppercase font-bold tracking-[10px] text-white text-[36px] md:px-[24px]">todo</h1>
   </div>;
 };
 
